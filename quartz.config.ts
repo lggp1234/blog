c790b15b630e009b6495e268e1dd8a044f0e31cf
Tashiwogege@gmail.com <95640393+lggp1234@users.noreply.github.com>
2025-07-14 19:51:36 +0900

    시도4

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,15 +8,16 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Quartz 4",
+    pageTitle: "MXPHY",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
-      provider: "plausible",
+      provider: "google",
+      tagId : "G-2Y502L9P1K",
     },
-    locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    locale: "ko-KR",
+    baseUrl: "maxedphysics.pages.dev",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
