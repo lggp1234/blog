7163b89438815adea799f24e2436e2c7a41098d0
lggp1234 <95640393+lggp1234@users.noreply.github.com>
2025-08-28 19:43:57 +0900

    Update quartz.layout.ts

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -28,14 +28,22 @@ export const sharedPageComponents: SharedLayout = {
 
 // components for pages that display a single page (e.g. a single note)
 export const defaultContentPageLayout: PageLayout = {
-  beforeBody: [
-    Component.ConditionalRender({
-      component: Component.Breadcrumbs(),
-      condition: (page) => page.fileData.slug !== "index",
+  beforeBody:[
+    Component.Flex({
+      components: [
+        {
+          Component: Component.ConditionalRender({
+            component: Component.Breadcrumbs(),
+            condition: (page) => page.fileData.slug !== "index",
+          }),
+          grow = true,
+        },
+        { Component: LanguageSwitch() },
+      ],
     }),
+
     Component.ArticleTitle(),
     Component.ContentMeta(),
-    LanguageSwitch(),
     Component.TagList(),
   ],
   left: [
