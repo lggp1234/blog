fc52849e9aaaa6f0143bef0e411d44f72309f8bb
lggp1234 <95640393+lggp1234@users.noreply.github.com>
2025-08-29 07:48:55 +0900

    Update quartz.layout.ts

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -1,6 +1,7 @@
 import { PageLayout, SharedLayout } from "./quartz/cfg"
 import * as Component from "./quartz/components"
 import LanguageSwitch from "./quartz/components/LanguageSwitch"
+import SpaLangGuard from "./quartz/components/SpaLangGuard"
 
 // components shared across all pages
 export const sharedPageComponents: SharedLayout = {
