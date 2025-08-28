065eaed87896bf2b6c6b35763b683ba58e447829
lggp1234 <95640393+lggp1234@users.noreply.github.com>
2025-08-28 16:54:03 +0900

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -24,8 +24,8 @@ const config: QuartzConfig = {
       fontOrigin: "googleFonts",
       cdnCaching: true,
       typography: {
-        header: "Schibsted Grotesk",
-        body: "Source Sans Pro",
+        header: "Libre Baskerville",
+        body: "Libre Baskerville",
         code: "IBM Plex Mono",
       },
       colors: {
